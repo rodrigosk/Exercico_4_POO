66c5e8fe2f10f8e0547f00516cf1ef8a465c9a86
Sousa <Sousa@192.168.1.3>
2015-11-17 00:44:25 -0200

    Exercicio 4 POO leitura de 2 arquivos e comparação dos mesmos

---
+++ src/exercicio_4/Inicio.java
@@ -0,0 +1,28 @@
+package exercicio_4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Inicio{
+	public static void main(String args[]){
+	File arquivo = new File("C:\\Rodrigo.txt");
+	
+	String linha;
+	
+	try{
+	BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(arquivo)));
+	while (br.ready()) {  
+		linha = br.readLine();  
+		System.out.println(linha);
+		}  
+		br.close();
+	}
+	catch(Exception e){
+		System.out.println("O arquivo não foi localizado");
+
+	}
+}
+}
+
